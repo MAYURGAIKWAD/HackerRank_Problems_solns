50e8f70427cbd0ac4a701f405572188b5367d2da
MAYURGAIKWAD <mayurgaikwad121@gmail.com>
2022-04-25 15:59:03 +0530

    Create Beautiful_triplets.py

---
+++ Easy/Beautiful_triplets.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    dic={}
+    count=0
+    for i in range(len(arr)):
+        if arr[i] not in dic.keys():
+            dic[arr[i]]=1
+    # print("the dict is: ", dic)
+    for i in range(len(arr)):
+        if (arr[i]+d in dic.keys()) and (arr[i]+2*d in dic.keys()):
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
